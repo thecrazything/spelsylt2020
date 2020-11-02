43ec32dfd451470915397f0cf731d7a7d3930fb7
joakim <joakim.bostrom43@gmail.com>
2020-11-02 22:45:07 +0100

    Added game-state management

---
+++ Assets/Scripts/GameManager/Character.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    private string _name = "TESTNAME";
+    private float _health;
+    private float _maxHealth = 100f;
+
+    public string name
+    {
+        get
+        {
+            return _name;
+        }
+    }
+
+    public float health
+    {
+        get
+        {
+            return _health;
+        }
+    }
+
+    public void subtractHealth(float value)
+    {
+        if (value < 0 )
+        {
+            throw new ArgumentException("Cannot subract a negative value. Use addHealth instead.")
+        }
+        _health -= value;
+        if (_health < 0)
+        {
+            _health = 0;
+        }
+
+
+        public void addHealth(float value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Cannot add a negative value. Use subctractHealth instead.")
+            }
+            _health += value;
+            if (_health > _maxHealth)
+            {
+                _health = _maxHealth;
+            }
+        }
+    }
---
+++ Assets/Scripts/GameManager/GameStatsService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatsService
+{
+    private static GameStatsService _serviceInstance;
+    private ICollection<Character> _characters;
+    public GameStats gameStats;
+    public ICollection<Character> characters
+    {
+        get
+        {
+            return _characters;
+        }
+    }
+    public ICharacter selectedCharacter;
+
+    public static GameStatsService Instance
+    {
+        get
+        {
+            if (_serviceInstance == null)
+            {
+                _serviceInstance = new GameStatsService();
+            }
+            return _serviceInstance;
+        }
+    }
+}
