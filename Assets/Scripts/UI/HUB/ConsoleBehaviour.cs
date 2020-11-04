672b53b638ac60e2c35749a329bf2b95ba81be72
joakim <joakim.bostrom43@gmail.com>
2020-11-04 18:34:19 +0100

    Moved console-text-animation to its own reusable class

--- Assets/Scripts/UI/HUB/ConsoleBehaviour.cs
+++ Assets/Scripts/UI/HUB/ConsoleBehaviour.cs
@@ -6,57 +6,33 @@ using UnityEngine.UI;
 public class ConsoleBehaviour : MonoBehaviour
 {
     public float writeDelay = 0.1f;
-    private Text _consoleText;
 
     private string defaultText = "Much need to be done in the HUB and on expeditions or whatever lol.";
     private string characterSelectedText = "{name} is feeling pretty swell. Pretty swiggity swooty. Hip with the kids.";
 
-    private int _currentPosition;
-    private string _textToWrite;
-
-    private float _currentWriteTime;
+    private TextPrintAnimation _textPrintAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
-        _consoleText = GetComponent<Text>();
-        _consoleText.text = defaultText;
+        _textPrintAnimation = new TextPrintAnimation(GetComponent<Text>(), writeDelay);
+        _textPrintAnimation.Write(defaultText);
         GameStatsService.Instance.onChangeSelectedCharacter += character =>
         {
             if (character == null)
             {
-                WriteText(defaultText);
+                _textPrintAnimation.Write(defaultText);
             } 
             else
             {
-                WriteText(characterSelectedText.Replace("{name}", character.name));
+                _textPrintAnimation.Write(characterSelectedText.Replace("{name}", character.name));
             }
         };
-    }
-
-    void WriteText(string text)
-    {
-        _consoleText.text = "";
-        _currentPosition = 0;
-        _textToWrite = text;
-        _currentWriteTime = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_textToWrite != null)
-        {
-            _currentWriteTime += Time.deltaTime;
-            if (_currentPosition >= _textToWrite.Length)
-            {
-                _textToWrite = null;
-            } 
-            else if (_currentWriteTime >= writeDelay)
-            {
-                _currentWriteTime = 0;
-                _consoleText.text += _textToWrite[_currentPosition++];
-            }
-        }
+        _textPrintAnimation.printNextIfTime(Time.deltaTime);
     }
 }
