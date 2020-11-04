9b6e384affa62a7a1c386d9f287f5db419ea5d54
Jacob Johansson <jikan_j@hotmail.com>
2020-11-04 20:37:50 +0100

    Interaction Component Movement

--- Assets/Scripts/Expedition/PlayerMovement.cs
+++ Assets/Scripts/Expedition/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public Animator animator;
+    public GameObject interactor;
 
     Vector2 movement;
 
@@ -21,25 +22,43 @@ public class PlayerMovement : MonoBehaviour
 
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
+        // Make sure the player only moves in one direction at a time
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            movement.x = 0;
+        }
+        else
+        {
+            movement.y = 0;
+        }
+
+        // Save last movement as current player rotation
         if (movement.magnitude > .1f) {
             animator.SetFloat("Last_Horizontal", movement.x);
             animator.SetFloat("Last_Vertical", movement.y);
+
+            UpdateInteractor(movement);
         }
     }
 
     void FixedUpdate()
     {
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-        {
-            movement.x = 0;
-        }
-        else {
-            movement.y = 0;
-        }
-
-        //movement = movement.normalized;
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
 
+    // Moves the interactor component to match player rotation
+    void UpdateInteractor(Vector2 dir)
+    {
+        if (dir.x > 0) {
+            interactor.transform.localPosition = new Vector3(.4f, 0, 0);
+        } else if (dir.x < 0) {
+            interactor.transform.localPosition = new Vector3(-.4f, 0, 0);
+        }
+        else if (dir.y > 0) {
+            interactor.transform.localPosition = new Vector3(0, .45f, 0);
 
+        } else {
+            interactor.transform.localPosition = new Vector3(0, -.45f, 0);
+        }
     }
 }
