3d757b6c19df2014ac989dbbc4cfb084a2c51f62
joakim <joakim.bostrom43@gmail.com>
2020-11-15 00:58:55 +0100

    Half-shadows and door-sized walls

--- Assets/Scripts/Shaders/TilemapShadowCaster2D.cs
+++ Assets/Scripts/Shaders/TilemapShadowCaster2D.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class TilemapShadowCaster2D : MonoBehaviour
@@ -14,14 +15,28 @@ public class TilemapShadowCaster2D : MonoBehaviour
         int i = 0;
         foreach (var position in tilemap.cellBounds.allPositionsWithin)
         {
-            if (tilemap.GetTile(position) == null)
+            var tile = tilemap.GetTile(position);
+            if (tile == null)
                 continue;
-
+            if (tile.name != "T_shadow_tile" && tile.name != "T_half_shadow_tile")
+            {
+                throw new ArgumentException("Tile must be of type T_shadow_tile or T_half_shadow_tile"); 
+            }
             GameObject shadowCaster = GameObject.Instantiate(shadowPrefab, shadowCasterContainer.transform);
             var corner = tilemap.CellToWorld(new Vector3Int(position.x, position.y, 1));
-            shadowCaster.transform.position = new Vector3(corner.x + (cellSize.x / 2), corner.y + (cellSize.y / 2), corner.z);
-            shadowCaster.transform.localScale = cellSize;
-            shadowCaster.name = "shadow_caster_" + i;
+            if (tile.name == "T_shadow_tile")
+            {
+                shadowCaster.transform.position = new Vector3(corner.x + (cellSize.x / 2), corner.y + (cellSize.y / 2), corner.z);
+                shadowCaster.transform.localScale = cellSize;
+                shadowCaster.name = "shadow_caster_" + i;
+            }
+            else
+            {
+                Vector3 halfSize = new Vector3(cellSize.x, cellSize.y / 2, cellSize.z);
+                shadowCaster.transform.position = new Vector3(corner.x + (halfSize.x / 2), corner.y + (halfSize.y * 1.5f), corner.z);
+                shadowCaster.transform.localScale = halfSize;
+                shadowCaster.name = "shadow_caster_half_" + i;
+            }
             i++;
         }
     }
