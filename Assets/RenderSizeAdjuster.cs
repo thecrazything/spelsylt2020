4ee2a06eccd96a8f68217c6b32c490d18be828e4
joakim <joakim.bostrom43@gmail.com>
2020-11-29 15:27:08 +0100

    CRT effect on expedition

---
+++ Assets/RenderSizeAdjuster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RenderSizeAdjuster : MonoBehaviour
+{
+    public Camera MainCamera;
+    public RawImage RenderImage;
+
+    private RenderTexture _texture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        _texture = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
+        _texture.Create();
+        MainCamera.targetTexture = _texture;
+        RenderImage.texture = _texture;
+    }
+}
